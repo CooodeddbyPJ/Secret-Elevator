d30aeb6f5f1113e22fccb75420bdfdc695e7ad8b
Phol Jamel <Phol Jamel@LAPTOP-363UL72C>
2025-08-10 17:45:18 +0800

    Add project files.

---
+++ Secret Elevator/Program.cs
@@ -0,0 +1,28 @@
+﻿Console.WriteLine("Welcome to the Secret Elevator!");
+Console.WriteLine("Please enter the secret code to access the elevator:");
+int secretCode = Convert.ToInt32(Console.ReadLine());
+if (secretCode == 135)
+{
+    Console.WriteLine("Welcome to First Floor!");
+
+}
+else if (secretCode == 246)
+{
+    Console.WriteLine("Welcome to Second Floor!");
+}
+else if (secretCode == 357)
+{
+    Console.WriteLine("Welcome to Third Floor!");
+}
+else if (secretCode == 999)
+{
+    Console.WriteLine("Welcome to Fourth Floor!");
+}
+else if (secretCode == 579)
+{
+    Console.WriteLine("Welcome to Fifth Floor!");
+}
+else
+{
+    Console.WriteLine("Access denied! Incorrect secret code.");
+}
